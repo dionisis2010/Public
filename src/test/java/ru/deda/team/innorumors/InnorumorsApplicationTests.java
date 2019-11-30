9e829dc123a5125e9201fb1e1e6b06b57638e297
dionisis2010 <2010dionisis2010@gmail.com>
2019-11-30 20:12:17 +0300

    create project

---
+++ src/test/java/ru/deda/team/innorumors/InnorumorsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.deda.team.innorumors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnorumorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
