b87fa9332ebd4ea3a23c70728361facbc907e42b
dionisis2010 <2010dionisis2010@gmail.com>
2019-12-09 15:31:36 +0300

    добавил пару обработчиков

--- src/main/java/ru/dedateam/innorumors/controller/HelloController.java
+++ src/main/java/ru/dedateam/innorumors/controller/HelloController.java
@@ -1,9 +1,12 @@
 package ru.dedateam.innorumors.controller;
 
+import javafx.geometry.Pos;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.dedateam.innorumors.data.entities.content.Post;
 
 @Controller
 @RequestMapping()
@@ -25,9 +28,15 @@ public class HelloController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "newpost")
-    public String createPost(Model model){
-
+    public String createPost(Post post){
+        System.out.println(post.getTitle());
         return "Innorumors/post-deatils";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "get")
+    public String get(Post post){
+        System.out.println(123);
+        return post.getTitle();
     }
 
 }
