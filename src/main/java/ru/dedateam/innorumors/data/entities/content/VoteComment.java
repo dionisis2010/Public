80a9c9a268b270eb4432b70a74c2c9985220fa0d
Zabidov <55929753+Zabidov@users.noreply.github.com>
2019-12-17 06:24:32 +0300

    миграции, репо и ентити на VotePost и VoteComment

--- src/main/java/ru/dedateam/innorumors/data/entities/content/VoteComment.java
+++ src/main/java/ru/dedateam/innorumors/data/entities/content/VoteComment.java
@@ -9,19 +9,19 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
-@Table(name = "post_votes")
-public class VotePost {
+@Table(name = "comment_votes")
+public class VoteComment {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_votes_seq")
-    @SequenceGenerator(name = "post_votes_seq", sequenceName = "post_votes_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comment_votes_seq")
+    @SequenceGenerator(name = "comment_votes_seq", sequenceName = "comment_votes_id_seq", allocationSize = 1)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
     @Column(name = "author", nullable = false)
     private Long authorId;
 
-    @Column(name = "post", nullable = false)
-    private Long postId;
+    @Column(name = "comment", nullable = false)
+    private Long commentId;
 
     @Column(name = "vote", nullable = false)
     @Enumerated(EnumType.STRING)
